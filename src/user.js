d55d26149bde51832dec8ea0a43b4753984a679d
EfeCanEKEN <135068937+EfeCanEKEN@users.noreply.github.com>
2026-01-29 14:57:01 +0000

    Add name property to User class

--- src/user.js
+++ src/user.js
@@ -1,17 +1,18 @@
 class User {
-  constructor(email, age, job, budget) {
-    this.email = email;
-    this.age = age;
-    this.job = job;
-    this.budget = budget;
+  constructor(name, email, age, job, budget) {
+    this.name = name
+    this.email = email
+    this.age = age
+    this.job = job
+    this.budget = budget
 
-    this.listings = [];
-    this.favorites = [];
+    this.listings = []
+    this.favorites = []
   }
 
   addListing(listing) {
-    this.listings.push(listing);
+    this.listings.push(listing)
   }
 }
 
-module.exports = User;
+module.exports = User
