9aadc11b6ee29be4730f522bd7cfdaf2fab64b60
EfeCanEKEN <135068937+EfeCanEKEN@users.noreply.github.com>
2026-02-24 10:06:11 +0000

    add database-connection module add routes for roomSeekerListing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+require('dotenv').config()
+require('./database-connection')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-const roomOfferListingRouter = require('./routes/roomOfferListing')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const roomOfferListingRouter = require('./routes/roomOfferListings')
+const roomSeekerListingRouter = require('./routes/roomSeekerListings')
 
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -22,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/roomOfferListing', roomOfferListingRouter)
+app.use('/roomOfferListings', roomOfferListingRouter)
+app.use('/roomSeekerListings', roomSeekerListingRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
